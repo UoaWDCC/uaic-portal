542a1550cb50d07df41ab841e76a6ebf675a9e90
Tarun <36789694+Ratchet7x5@users.noreply.github.com>
2024-06-14 14:14:49 +0530

    change import in getUsers.ts (api)

--- api/gateway/getUsers.ts
+++ api/gateway/getUsers.ts
@@ -1,6 +1,6 @@
-import { users } from "../schemas/user";
 import { db } from "../db/config/db";
 import { User } from "../types/types";
+import { users } from "../schemas/user";
 
 export async function getUsers() {
   return (await db.select().from(users)) as User[];
