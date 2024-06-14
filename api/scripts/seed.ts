4edc09ebe5d9db73a5780c789d840946bc31ff21
Tarun <36789694+Ratchet7x5@users.noreply.github.com>
2024-06-14 13:21:54 +0530

    update seed.ts data

--- api/scripts/seed.ts
+++ api/scripts/seed.ts
@@ -1,4 +1,4 @@
-import { users } from "../schemas/User";
+import { users } from "../schemas/user";
 import { db } from "../db/config/db";
 
 const main = async () => {
@@ -9,22 +9,46 @@ const main = async () => {
     await db.insert(users).values([
       {
         name: "Harsheel Singh",
-        description: "test",
         email: "hsin212@aucklanduni.ac.nz",
         year_of_study: 4,
+        university_id: "12345",
+        upi: "hsin212",
+        institution: "UoA",
+        study_field: "SOFTENG",
+        is_admin: true,
+        is_paid: true,
+        is_info_confirmed: true,
+        created_at: new Date(),
+        membership_expiry: null,
       },
       {
         name: "Guryash Matharu",
-        description: "test",
         email: "gmat222@aucklanduni.ac.nz",
         year_of_study: 4,
+        university_id: "23456",
+        upi: "gmat222",
+        institution: "UoGOAT",
+        study_field: "SOFTENG",
+        is_admin: true,
+        is_paid: true,
+        is_info_confirmed: false,
+        created_at: new Date(),
+        membership_expiry: null,
       },
       {
         name: "Naren Rohan",
-        description: "test",
         email: "nroh555@aucklanduni.ac.nz",
         year_of_study: 4,
-      },
+        university_id: "",
+        upi: "",
+        institution: "",
+        study_field: "SOFTENG",
+        is_admin: true,
+        is_paid: true,
+        is_info_confirmed: false,
+        created_at: new Date(),
+        membership_expiry: null,
+      }
     ]);
     process.exit(0);
   } catch (error) {
