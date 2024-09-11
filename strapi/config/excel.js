763ebb20688f249e12daea17db8a5d1b243ba84d
Guryash <gmat224@aucklanduni.ac.nz>
2024-09-11 14:56:40 +1200

    feat: add export packages to strapi

---
+++ strapi/config/excel.js
@@ -0,0 +1,27 @@
+module.exports = {
+  config: {
+    "api::previous-team.previous-team": {
+      columns: [
+        "id",
+				"Name",
+				"Role",
+				"Year"
+      ],
+      relation: {
+      },
+      locale: "false",
+    },
+    "api::exec.exec": {
+			columns: [
+				"id",
+				"Name",
+				"Position",
+				"Description",
+				"Role",
+      ],
+      relation: {
+      },
+      locale: "false",
+    },
+  },
+};
