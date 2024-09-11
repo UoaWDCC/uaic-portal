825db0bede2aa1cb06fe865e1d83423866932e88
Devesh Duptala <dduptala@gmail.com>
2024-09-12 00:48:07 +1200

    ♻️ Refactor: Added header

--- web/src/screens/DetailedEventScreen.tsx
+++ web/src/screens/DetailedEventScreen.tsx
@@ -1,3 +1,4 @@
+import Header from "@components/Header";
 import React from "react";
 import { useLocation } from "react-router-dom";
 
@@ -10,12 +11,23 @@ const DetailedEventScreen = () => {
   }
 
   return (
-    <div>
-      <h1>{event.title}</h1>
-      <p>{event.description}</p>
-      <img src={event.image} alt="" />
-      {/* Add more details here */}
-      {/* Example: <img src={event.imageUrl} alt={event.title} /> */}
+    <div className="h-auto">
+      <div className="max-w-screen h-auto bg-white">
+        <div className="max-w-screen from-AUIS-dark-teal to-AUIS-teal h-auto bg-gradient-to-b">
+          <Header />
+          <div className="flex h-auto w-full flex-row items-center justify-center bg-red-500 bg-transparent pb-10">
+            <div className="flex w-11/12 flex-col lg:w-3/4">
+              <div>
+                <img src={event.image} width={100} alt="" />
+              </div>
+              <div>
+                <h1>{event.title}</h1>
+                <p>{event.description}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
